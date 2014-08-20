5a2d67866debaa3be78dcdebe6ab0cc17c365cbf
root <root@ra.localhost>
2014-08-20 19:39:07 -0400

    Committer: Frank Caviggia <fcaviggi@redhat.com>
           new file:   config/isolinux/grub.conf
           new file:   config/isolinux/isolinux.cfg
           new file:   config/stig-fix/classification-banner.py
           new file:   config/stig-fix/dod_firefox_config.tar.gz
           new file:   config/stig-fix/menu.py
           new file:   config/stig-fix/scap-security-guide-0.1.17-1.20140524git2eeeca7.el6.noarch.rpm
           new file:   config/stig-fix/stig-fix-1.7.8-el6.noarch.rpm
           new file:   config/stig-fix/stig-fix.cfg
           new file:   createiso.sh

---
+++ config/stig-fix/classification-banner.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+# Classification Banner
+# Author: Frank Caviggia <fcaviggia at gmail.com>
+# Copyright: Frank Caviggia, 2013
+# Version: 1.5
+# License: GPLv2
+
+import sys
+import os
+import optparse
+
+try:
+    os.environ['DISPLAY']
+    import pygtk
+    import gtk
+except:
+    print("Error: DISPLAY environment varible not set.")
+    sys.exit(1)
+
+# Classifion Banner Class
+class Classification_Banner:
+    """Class to create and refresh the actual banner."""
+
+    def __init__(self, message="UNCLASSIFIED", fgcolor="#000000",
+        bgcolor="#00CC00", face="liberation-sans", size="small",
+        weight="bold"):
+        """Set up and display the main window
+
+        Keyword arguments:
+        message -- The classification level to display
+        fgcolor -- Foreground color of the text to display
+        bgcolor -- Background color of the banner the text is against
+        face    -- Font face to use for the displayed text
+        size    -- Size of font to use for text
+        weight  -- Bold or normal
+        """
+    	# Dynamic Resolution Scaling
+    	self.monitor = gtk.gdk.Screen()
+	self.monitor.connect("size-changed", self.resize)
+	# Newer versions of pygtk have this method
+	try:
+		self.monitor.connect("monitors-changed", self.resize)
+	except:
+		pass
+
+        # Create Main Window
+        self.window = gtk.Window()
+        self.window.set_position(gtk.WIN_POS_CENTER)
+        self.window.connect("hide", self.restore)
+        self.window.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse(bgcolor))
+        self.window.set_property('skip-taskbar-hint', True)
+        self.window.set_property('skip-pager-hint', True)
+        self.window.set_property('destroy-with-parent', True)
+        self.window.stick()
+        self.window.set_decorated(False)
+        self.window.set_keep_above(True)
+        self.window.set_app_paintable(True)
+        # Try Xrandr to determine primary monitor resolution
+    	try:
+                self.screen = os.popen("xrandr | grep *0 | awk '{ print $2$3$4 }'").readlines()[0]
+                self.hres = self.screen.split('x')[0]
+                self.vres = self.screen.split('x')[1].split('+')[0]
+	except:
+		try:
+	    		self.screen = os.popen("xrandr | grep primary | awk '{ print $4 }'").readlines()[0]
+	    		self.hres = self.screen.split('x')[0]
+	    		self.vres = self.screen.split('x')[1].split('+')[0]
+		except:
+			self.screen = os.popen("xrandr | grep connected | awk '{ print $3 }'").readlines()[0]
+			self.hres = self.screen.split('x')[0]
+			self.vres = self.screen.split('x')[1].split('+')[0]
+        	else:
+			# Failback to GTK method
+			self.display = gtk.gdk.display_get_default()
+			self.screen = self.display.get_default_screen()
+			self.hres = self.screen.get_width()
+			self.vres = self.screen.get_height()
+        self.window.set_default_size(int(self.hres), 5)
+
+        # Create Main Vertical Box to Populate
+        self.vbox = gtk.VBox()
+
+        self.label = gtk.Label(
+            "<span font_family='%s' weight='%s' foreground='%s' size='%s'>%s</span>" %
+            (face, weight, fgcolor, size, message))
+        self.label.set_use_markup(True)
+        self.label.set_justify(gtk.JUSTIFY_CENTER)
+        self.vbox.pack_start(self.label, True, True, 0)
+
+        self.window.add(self.vbox)
+        self.window.show_all()
+        self.width, self.height = self.window.get_size()
+
+    # Restore Minimized Window
+    def restore(self, widget, data=None):
+        self.window.present()
+        return True
+
+    # Destroy Classification Banner Window on Resize (Display Banner Will Relaunch)
+    def resize(self, widget, data=None):
+        self.window.destroy()
+        return True
+
+class Display_Banner:
+    """Display Classification Banner Message"""
+
+    def __init__(self):
+
+    	# Dynamic Resolution Scaling
+    	self.monitor = gtk.gdk.Screen()
+	self.monitor.connect("size-changed", self.resize)
+	# Newer versions of pygtk have this method
+	try:
+		self.monitor.connect("monitors-changed", self.resize)
+	except:
+		pass
+
+	# Launch Banner
+	self.config, self.args = self.configure()
+	self.execute(self.config)
+
+    # Read Global configuration
+    def configure(self):
+        config = {}
+        try:
+        	execfile("/etc/classification-banner", config)
+        except:
+        	pass
+        defaults = {}
+        defaults["message"] = config.get("message", "UNCLASSIFIED")
+        defaults["fgcolor"] = config.get("fgcolor", "#000000")
+        defaults["bgcolor"] = config.get("bgcolor", "#00CC00")
+        defaults["face"] = config.get("face", "liberation-sans")
+        defaults["size"] = config.get("size", "small")
+        defaults["weight"] = config.get("weight", "bold")
+        defaults["show_top"] = config.get("show_top", True)
+        defaults["show_bottom"] = config.get("show_bottom", True)
+
+        # Use the global config to set defaults for command line options
+        parser = optparse.OptionParser()
+        parser.add_option("-m", "--message", default=defaults["message"],
+            help="Classification message")
+        parser.add_option("-f", "--fgcolor", default=defaults["fgcolor"],
+            help="Foreground (text) color")
+        parser.add_option("-b", "--bgcolor", default=defaults["bgcolor"],
+            help="Background color")
+        parser.add_option("--face", default=defaults["face"], help="Font face")
+        parser.add_option("--size", default=defaults["size"], help="Font size")
+        parser.add_option("--weight", default=defaults["weight"],
+            help="Font weight")
+        parser.add_option("--hide-top", default=defaults["show_top"],
+            dest="show_top", action="store_false",
+            help="Disable the top banner")
+        parser.add_option("--hide-bottom", default=defaults["show_bottom"],
+            dest="show_bottom", action="store_false",
+            help="Disable the bottom banner")
+
+        options, args = parser.parse_args()
+	return options, args
+
+    # Launch the Classification Banner Window(s)
+    def execute(self, options):
+        if options.show_top:
+            top = Classification_Banner(
+                options.message,
+                options.fgcolor,
+                options.bgcolor,
+                options.face,
+                options.size,
+                options.weight)
+            top.window.move(0, 0)
+        if options.show_bottom:
+            bottom = Classification_Banner(
+                options.message,
+                options.fgcolor,
+                options.bgcolor,
+                options.face,
+                options.size,
+                options.weight)
+            bottom.window.move(0, int(bottom.vres))
+
+    # Relauch the Classification Banner on Screen Resize
+    def resize(self, widget, data=None):
+	self.config, self.args = self.configure()
+	self.execute(self.config)
+        return True
+
+
+# Main Program Loop
+if __name__ == "__main__":
+	run = Display_Banner()
+	gtk.main()
---
+++ createiso.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+###############################################################################
+# STIG FIX DVD CREATOR
+#
+# This script was written by Frank Caviggia, Red Hat Consulting
+# Last update was 21 January 2014
+# This script is NOT SUPPORTED by Red Hat Global Support Services.
+# Please contact Josh Waldman for more information.
+#
+# Author: Frank Caviggia (fcaviggi@redhat.com)
+# Copyright: Red Hat, (c) 2013
+# Version: 1.1.3
+# License: GPLv2
+# Description: Kickstart Installation of RHEL 6 with DISA STIG 
+###############################################################################
+
+# GLOBAL VARIABLES
+DIR=`pwd`
+
+# USAGE STATEMENT
+function usage() {
+cat << EOF
+usage: $0 rhel-server-6.5-x86_64-dvd.iso
+
+DISA STIG Installer Kickstart RHEL 6.4+
+
+Customizes a RHEL 6.4+ x86_64 Server or Workstation DVD to install
+with the following hardening:
+
+  - DISA STIG for Red Hat Enterprise Linux
+  - DISA STIG for Firefox (User/Developer Workstation)
+  - Classification Banner (Graphical Desktop)
+
+EOF
+}
+
+while getopts ":vhq" OPTION; do
+	case $OPTION in
+		h)
+			usage
+			exit 0
+			;;
+		?)
+			echo "ERROR: Invalid Option Provided!"
+			echo
+			usage
+			exit 1
+			;;
+	esac
+done
+
+# Check for root user
+if [[ $EUID -ne 0 ]]; then
+	if [ -z "$QUIET" ]; then
+		echo
+		tput setaf 1;echo -e "\033[1mPlease re-run this script as root!\033[0m";tput sgr0
+	fi
+	exit 1
+fi
+
+# Determine if DVD is Bootable
+`file $1 | grep 9660 | grep -q bootable`
+if [[ $? -eq 0 ]]; then
+	echo "Mounting RHEL DVD Image..."
+	mkdir -p /rhel
+	mkdir $DIR/rhel-dvd
+	mount -o loop $1 /rhel
+	echo "Done."
+	# Tests DVD for RHEL 6.4+
+	if [[ $(grep "Red Hat" /rhel/.discinfo | awk '{ print $5 }' | awk -F '.' '{ print $1 }') -ne 6 ]]; then
+		echo "ERROR: Image is not RHEL 6.4+"
+		umount /rhel
+		rm -rf /rhel
+		exit 1
+	fi
+	if [[ $(grep "Red Hat" /rhel/.discinfo | awk '{ print $5 }' | awk -F '.' '{ print $2 }') -lt 4 ]]; then
+		echo "ERROR: Image is not RHEL 6.4+"
+		umount /rhel
+		rm -rf /rhel
+		exit 1
+	fi
+	echo -n "Copying RHEL DVD Image..."
+	cp -a /rhel/* $DIR/rhel-dvd/
+	cp -a /rhel/.discinfo $DIR/rhel-dvd/
+	echo " Done."
+	umount /rhel
+	rm -rf /rhel
+else
+	echo "ERROR: ISO image is not bootable."
+	exit 1
+fi
+
+echo -n "Modifying RHEL DVD Image..."
+cp -a $DIR/config/* $DIR/rhel-dvd/
+echo " Done."
+
+echo "Remastering RHEL DVD Image..."
+cd $DIR/rhel-dvd
+chmod a+w isolinux/isolinux.bin
+find . -name TRANS.TBL -exec rm '{}' \; 
+/usr/bin/mkisofs -J -T -o $DIR/rhel-stig-fix.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -m TRANS.TBL .
+cd $DIR
+rm -rf $DIR/rhel-dvd
+echo "Done."
+
+echo "Signing RHEL DVD Image..."
+/usr/bin/implantisomd5 $DIR/rhel-stig-fix.iso
+echo "Done."
+
+echo "DVD Created. [rhel-stig-fix.iso]"
+
+exit 0
